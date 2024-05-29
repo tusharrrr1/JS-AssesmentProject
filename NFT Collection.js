735eb6abfb3bee07efbd2a2a8e4b4c356753c856
tusharrrr1 <161235330+tusharrrr1@users.noreply.github.com>
2024-05-29 23:47:20 +0530

    Create NFT Collection.js

---
+++ NFT Collection.js
@@ -0,0 +1,57 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+const NFTs=[];
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (Name, Description, Price, Image) {
+  const NFT = 
+    {
+      Name: Name,
+      Description: Description,
+      Price: Price,
+      Image: Image
+    }
+    NFTs.push(NFT);
+    console.log("minted: ", Name);
+  }
+  
+
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+  for(let i=0; i<NFTs.length; i++){
+    console.log("\nID: " + (i+1));
+    console.log("\nname: " + NFTs[i].Name);
+    console.log("\nDescription: " + NFTs[i].Description);
+    console.log("\nPrice: " + NFTs[i].Price);
+    console.log("\nImage: " + NFTs[i].Image);
+  }
+
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+  console.log(NFTs.length);
+
+}
+
+// call your functions below this line
+mintNFT("NeedForSpeed", "Playground Simulation","750$", "applicable")
+mintNFT("CallOfDuty", "War Simulation","1000$", "applicable")
+mintNFT("FIFA", "Football Simulation","500$", "applicable")
+mintNFT("GTA", "City Simulation","1500$", "applicable") 
+listNFTs();
+getTotalSupply();
+
